a8fbe2551b9ba167883762fdf4b1bb14d00b3def
joekendal <13680617+joekendal@users.noreply.github.com>
2020-11-15 03:07:05 +0000

    Added VPC

---
+++ infra/network.tf
@@ -0,0 +1,19 @@
+resource "aws_vpc" "main" {
+  cidr_block           = "10.1.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-vpc")
+  )
+}
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    local.common_tags,
+    map("Name", "${local.prefix}-main")
+  )
+}
