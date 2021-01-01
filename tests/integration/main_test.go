0690c116b4bc28385e3ea5affe857e7db19fbe06
joekendal <13680617+joekendal@users.noreply.github.com>
2021-01-01 13:44:44 +0000

    Fixed naming prefix to prevent conflicts

--- tests/integration/main_test.go
+++ tests/integration/main_test.go
@@ -1,8 +1,12 @@
 package test
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
+	"github.com/gruntwork-io/terratest/modules/aws"
+	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -10,11 +14,24 @@ import (
 func TestMain(t *testing.T) {
 	t.Parallel()
 
+	expectedName := fmt.Sprintf("terratest-aws-main-%s", strings.ToLower(random.UniqueId()))
+	awsRegion := aws.GetRandomStableRegion(t, nil, nil)
+
 	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../../infra/",
+
+		Vars: map[string]interface{}{
+			"prefix": expectedName,
+			"region": awsRegion,
+		},
+
+		// Environment variables to set when running Terraform
+		EnvVars: map[string]string{
+			"AWS_DEFAULT_REGION": awsRegion,
+		},
 	})
 
 	// Clean up resources with "terraform destroy" at the end of the test.
@@ -25,5 +42,5 @@ func TestMain(t *testing.T) {
 
 	// Run `terraform output` to get the values of output variables and check they have the expected values.
 	output := terraform.Output(t, terraformOptions, "region")
-	assert.Equal(t, "eu-west-2", output)
+	assert.Equal(t, awsRegion, output)
 }
