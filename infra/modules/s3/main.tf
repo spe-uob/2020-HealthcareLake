870672265f57c034e123c119b17976e969bc9ca3
joekendal <13680617+joekendal@users.noreply.github.com>
2021-02-08 03:28:56 +0000

    Added HTTPS-only restriction on S3 bucket (FHIR) #107

--- infra/modules/s3/main.tf
+++ infra/modules/s3/main.tf
@@ -72,6 +72,37 @@ resource "aws_s3_bucket" "fhir_binary" {
   }
 }
 
+resource "aws_s3_bucket_policy" "https_only_policy" {
+  bucket = aws_s3_bucket.fhir_binary.id
+  policy = data.aws_iam_policy_document.s3_https_only.json
+}
+
+data "aws_iam_policy_document" "s3_https_only" {
+  statement {
+    sid = "AllowSSLRequestsOnly"
+
+    effect = "Deny"
+
+    principals {
+       type = "*"
+       identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+
+    resources = [
+      aws_s3_bucket.fhir_binary.arn,
+      "${aws_s3_bucket.fhir_binary.arn}/*"
+    ]
+
+    condition {
+      test  = "BoolLike"
+      variable = "aws:SecureTransport"
+      values = [false]
+    }
+  }
+}
+
 resource "aws_s3_bucket" "fhir_log_bucket" {
   bucket = "${var.prefix}-fhir-binary-logs"
   acl    = "log-delivery-write"
