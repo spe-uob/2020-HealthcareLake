a2e3a33fb94d1c0406fdee562c907b2147f15c74
joekendal <13680617+joekendal@users.noreply.github.com>
2021-02-17 16:43:02 +0000

    Removed node_modules

--- api/dist/node_modules/aws-xray-sdk-core/lib/patchers/promise_p.d.ts
+++
@@ -1,4 +0,0 @@
-export const capturePromise: {
-  (): void;
-  patchThirdPartyPromise(Promise: any): void;
-};
--- api/dist/node_modules/aws-xray-sdk-core/lib/segments/plugins/ecs_plugin.d.ts
+++
@@ -1,9 +0,0 @@
-export interface ECSMetadata {
-  ecs: {
-    container: string;
-  };
-}
-
-export function getData(callback: (metadata?: ECSMetadata) => void): void;
-
-export const originName: string;
